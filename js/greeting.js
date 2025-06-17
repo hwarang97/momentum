a43ed698d7210f8bce1fdc32baebee451751b579
dolggul2 <dolggul2@gmail.com>
2025-06-18 00:46:20 +0900

    feat: add login-form and load username.

--- js/greeting.js
+++ js/greeting.js
@@ -1,8 +1,17 @@
 const loginForm = document.querySelector('#login-form');
+const greeting = document.querySelector('#greeting');
 
 function onLoginSubmit(event){
     event.preventDefault();
-    
+    const name = loginForm.querySelector('input');
+    const userName = name.value;
+    localStorage.setItem('username', userName);
+    greeting.innerText = `Hello, ${userName}!`;
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
+
+const savedUserName = localStorage.getItem('username');
+if(savedUserName){
+    greeting.innerText = savedUserName;
+}
