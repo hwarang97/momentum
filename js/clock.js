fddeded053afd2d3f7ce7656d6f83b64d4026f0d
dolggul2 <dolggul2@gmail.com>
2025-06-18 16:29:01 +0900

    feat: add formatTime function for padding time value with 0.

--- js/clock.js
+++ js/clock.js
@@ -2,10 +2,15 @@ const clock = document.querySelector('#clock');
 
 function updateClock(){
     const now = new Date();
-    const hours = now.getHours();
-    const minutes = now.getMinutes();
-    const seconds = now.getSeconds();
+    const hours = formatTime(now.getHours());
+    const minutes = formatTime(now.getMinutes());
+    const seconds = formatTime(now.getSeconds());
     clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
+function formatTime(time){
+    return String(time).padStart(2, '0');
+}
+
 updateClock();
+setInterval(updateClock, 1000);
